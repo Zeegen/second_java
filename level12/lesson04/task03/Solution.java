e42729b24445836a54737a58ba9a30836dfcecd2
Zeegen <dogy@Nikolajs-MacBook-Pro.local>
2017-01-29 03:06:46 +0200

    go go

---
+++ level12/lesson04/task03/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    //Напишите тут ваши методы
+    public void print(int a){}
+    public void print(Integer c){}
+    public void print(String d){}
+    public void print(int a, int b){}
+    public void print(String d, int a){}
+
+}

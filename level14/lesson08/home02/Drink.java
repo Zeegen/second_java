b628cc40c75f306ee8d754493dbcc65a2ba4d856
Zeegen <dogy@Nikolajs-MacBook-Pro.local>
2017-02-01 00:27:11 +0200

    my variant
    помог интнли

---
+++ level14/lesson08/home02/Drink.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by dogy on 31.01.17.
+ */
+public abstract class Drink
+{
+    public void taste()
+    {
+        System.out.println("Вкусно");
+    }
+}
---
+++ level14/lesson08/home02/Wine.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by dogy on 31.01.17.
+ */
+public class Wine extends Drink
+{
+    public String getHolidayName()
+    {
+        return "День рождения";
+    }
+}
+
